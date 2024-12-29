1a2fff2cfc5c5985862e91c6764dd8ab83e11c79
u-Hoshi <yuta000707@yahoo.co.jp>
2024-12-29 22:17:29 +0900

    geminiとの疎通確認

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import GeminiChatBox from "@/components/features/top/GeminiChatBox";
 
 export default async function Home() {
   return (
     <>
       <main className="flex-1 flex flex-col gap-6 px-4">
         <h1>日報応援アプリ</h1>
+        <GeminiChatBox />
       </main>
     </>
   );
---
+++ components/features/top/GeminiChatBox.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useState } from "react";
+import axios from "axios";
+
+const prompt = "アメリカと日本の時差を教えて";
+
+const GeminiChatBox = () => {
+  const [geminiResponse, setGeminiResponse] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+
+  const handleSubmitPrompt = async () => {
+    setIsLoading(true);
+    setError("");
+
+    try {
+      const response = await axios.post("/api/gemini-api", {
+        prompt_post: prompt,
+      });
+      setGeminiResponse(response.data.text);
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        setError(err.response?.data?.message || "APIの呼び出しに失敗しました");
+      } else {
+        setError("予期せぬエラーが発生しました");
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleSubmitPrompt}>Gemini APIを叩く</button>
+      {isLoading ? (
+        <div className="flex justify-center" aria-label="読み込み中">
+          <div className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"></div>
+        </div>
+      ) : (
+        <div> {geminiResponse || error}</div>
+      )}
+    </div>
+  );
+};
+
+export default GeminiChatBox;
